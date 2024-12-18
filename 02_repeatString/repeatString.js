ae0d4740251550b70f75ee883620a56bf7ba4f45
Kyle Belanger <Kyleb44@hotmail.com>
2024-12-18 15:59:14 -0500

    complete lesson # 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return "ERROR"
+  }
+
+  array = [];
+  
+  for(let i = 0 ; i < num; i++){
+    array.push(string);
+  }
+
+  return array.join("");
 
 };
 

1cc9305a1c5cb10d77afe7f914d23f3e559213e8
Kyle Belanger <Kyleb44@hotmail.com>
2024-12-18 16:35:53 -0500

    complete lesson 3
    I used WAY more lines then needed to solve this

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(text) {
+  length = text.length;
 
+  reversedString = "";
+
+  for (let i = 0; i < length; i++ ){
+    char = text[i]
+
+    reversedString = char + reversedString
+  }
+
+  return reversedString
 };
 
 // Do not edit below this line

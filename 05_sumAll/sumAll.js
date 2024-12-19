e6cde00a1765b42445254d9ee3bf48ce62b67e4f
Kyle Belanger <Kyleb44@hotmail.com>
2024-12-19 06:54:47 -0500

    complete lesson 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if(num1 < 0 || !Number.isInteger(num1) || 
+      num2 < 0 || !Number.isInteger(num2)) {
+        return "ERROR";
+      }
 
+  finalSum = 0;
+  firstNum = num1;
+  secondNum = num2;
+
+  if (num1 > num2) {
+    firstNum = num2;
+    secondNum = num1;
+  }
+
+  for (i = firstNum; i < secondNum + 1; i++){
+
+    finalSum += i
+  }
+  return finalSum
 };
 
 // Do not edit below this line
